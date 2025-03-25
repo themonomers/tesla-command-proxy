62b8b0e969f7e8dfdf3c29611337943fe8fc5c41
themonomers <54368885+themonomers@users.noreply.github.com>
2025-03-25 09:02:17 -0700

    Create monitor_proxy.sh
    Add bash script to check from crontab if the Tesla Http Command Proxy is down.

---
+++ monitor_proxy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+processes="$(ps -ef | grep tesla-http-proxy | grep -v grep)"
+up=false
+
+# loop through filtered processes
+while delimiter='/' read -ra row; do
+
+  for i in "${row[@]}"; do
+#    echo "($up) $i"
+    if [[ $i == *"tesla-http-proxy"* ]]; then
+      up=true
+    fi
+  done
+done <<< "$processes"
+
+# send notification if process found to be not running
+if [ "$up" == "false" ]; then
+#  echo "Tesla HTTP Proxy not running"
+  curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data '{"content":"'"Tesla HTTP Proxy not running"'"}' $webhook &> /dev/null
+fi
