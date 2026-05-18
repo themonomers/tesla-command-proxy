894fbf428ad072ef49d6c232d9e7841f312619a8
themonomers <54368885+themonomers@users.noreply.github.com>
2026-05-17 18:34:52 -0700

    Updated for folder structure changes

--- run_tesla_command_proxy.sh
+++ run_tesla_command_proxy.sh
@@ -2,4 +2,4 @@
 
 cd /home/pi/tesla-command-proxy
 
-/usr/local/go/bin/go run /home/pi/tesla-command-proxy/cmd/tesla-http-proxy -tls-key /home/pi/tesla/python/common/key.pem -cert /home/pi/tesla/python/common/cert.pem -port 4443 -key-file /home/pi/tesla/python/common/tesla_private_key.pem -mode owner $1 > tesla-command-proxy.log 2>&1 &
+/usr/local/go/bin/go run /home/pi/tesla-command-proxy/cmd/tesla-http-proxy -tls-key /home/pi/tesla/python/data/keys/key.pem -cert /home/pi/tesla/python/data/keys/cert.pem -port 4443 -key-file /home/pi/tesla/python/data/keys/tesla_private_key.pem -mode owner $1 > tesla-command-proxy.log 2>&1 &
